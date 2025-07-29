38e82b610b3437f27d42124a83cb90ddde30adaa
doz <doz@dozdeMacBook-Pro.local>
2025-07-29 15:03:53 +0800

    docs: Add documentation for functions in ord.go

--- hyperidx/ord.go
+++ hyperidx/ord.go
@@ -4,10 +4,13 @@ import (
 	"github.com/hootuu/helix/storage/hdb"
 	"github.com/hootuu/helix/storage/hmeili"
 	"github.com/hootuu/hyle/hlog"
+	"github.com/hootuu/hyle/hypes/collar"
 	"github.com/hootuu/hyper/hiorder"
+	"github.com/hootuu/hyper/hpay/payment"
 	"github.com/hootuu/hyper/hshipping/shipping"
 	"github.com/hootuu/hyper/hyperplt"
 	"github.com/meilisearch/meilisearch-go"
+	"github.com/nineora/harmonic/nineidx"
 	"go.uber.org/zap"
 )
 
@@ -74,12 +77,20 @@ func (idx *TxOrdIndexer) Load(autoID int64) (hmeili.Document, error) {
 	doc := hmeili.NewMapDocument(m.ID, m.AutoID, m.UpdatedAt.UnixMilli())
 	doc["code"] = m.Code
 	doc["title"] = m.Title
-	doc["payer"] = m.Payer.MustToDict()
+	if m.Payer != "" {
+		doc["payer"] = idx.GetPayerDigest(m.Payer)
+	}
 	doc["payee"] = m.Payee.MustToDict()
 	doc["amount"] = m.Amount
 	doc["status"] = m.Status
 	doc["matter"] = m.Matter
 	doc["payment_id"] = m.PaymentID
+	if m.PaymentID != 0 {
+		doc["payment"], err = idx.GetPaymentDigest(m.PaymentID)
+		if err != nil {
+			return nil, err
+		}
+	}
 	doc["shipping_id"] = m.ShippingID
 	if m.ShippingID != 0 {
 		doc["shipping"], err = idx.GetShippingDigest(m.ShippingID)
@@ -95,13 +106,37 @@ func (idx *TxOrdIndexer) Load(autoID int64) (hmeili.Document, error) {
 	return doc, nil
 }
 
+func (idx *TxOrdIndexer) GetPayerDigest(payer collar.Link) map[string]any {
+	payerM := payer.MustToDict()
+	return map[string]any{
+		"code": payerM.Code,
+		"id":   payerM.ID,
+		"info": nineidx.FastGetSattva(payerM.ID),
+	}
+}
+
 func (idx *TxOrdIndexer) GetShippingDigest(shippingID shipping.ID) (map[string]any, error) {
 	shipM, err := hdb.MustGet[shipping.ShipM](hyperplt.DB(), "id = ?", shippingID)
 	if err != nil {
 		return nil, err
 	}
 	return map[string]any{
-		"uni_link": shipM.UniLink.MustToDict(),
-		"address":  shipM.Address,
+		"uni_link":     shipM.UniLink.MustToDict(),
+		"address":      shipM.Address,
+		"courier_code": shipM.CourierCode,
+		"tracking_no":  shipM.TrackingNo,
+		"shipped_at":   shipM.ShippedAt,
+	}, nil
+}
+
+func (idx *TxOrdIndexer) GetPaymentDigest(paymentID payment.ID) (map[string]any, error) {
+	payM, err := hdb.MustGet[payment.JobM](hyperplt.DB(), "payment_id = ? AND payment_seq = ?", paymentID, 1)
+	if err != nil {
+		return nil, err
+	}
+	return map[string]any{
+		"status": payM.Status,
+		"ctx":    payM.Ctx,
+		"pay_no": payM.PayNo,
 	}, nil
 }
