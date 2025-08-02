1750e1e9f2187601db59d55177b657b71b103cc7
wnjie <13296719323@163.com>
2025-08-02 17:06:54 +0800

    fix order

--- hiorder/use_engine.go
+++ hiorder/use_engine.go
@@ -5,3 +5,7 @@ import "context"
 func (e *Engine[T]) AdvToCompleted(ctx context.Context) error {
 	return e.doAdvToCompleted(ctx)
 }
+
+func (e *Engine[T]) AdvToCanceled(ctx context.Context) error {
+	return e.doAdvToCanceled(ctx)
+}
