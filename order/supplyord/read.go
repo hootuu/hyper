c752bfc2da15e299d2de125dd4b06c524f3bf800
wnjie <13296719323@163.com>
2025-09-05 16:01:13 +0800

    fix ord

--- order/supplyord/read.go
+++ order/supplyord/read.go
@@ -5,8 +5,18 @@ import (
 	"github.com/hootuu/helix/storage/hdb"
 	"github.com/hootuu/hyper/hiorder"
 	"github.com/hootuu/hyper/hyperplt"
+	"github.com/spf13/cast"
 )
 
 func GetByProdOrderID(ctx context.Context, prodOrderID hiorder.ID) (*hiorder.Order[Matter], error) {
 	return hdb.Get[hiorder.Order[Matter]](hyperplt.DB(), "link = ?", GetFactory().core.OrderCollar(prodOrderID).Link())
+}
+
+func GetProdOrdID(ctx context.Context, supplyOrdId hiorder.ID) (hiorder.ID, error) {
+	supOrd, err := hdb.MustGet[hiorder.Order[Matter]](hyperplt.DB(), "id = ?", supplyOrdId)
+	if err != nil {
+		return 0, nil
+	}
+	meta := supOrd.Ex.Meta
+	return cast.ToUint64(meta.Get("orderId").String()), nil
 }
