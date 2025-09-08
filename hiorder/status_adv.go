2500d16f625848ef5cab28fd8b5cd082b6af7cfe
doz <doz@dozdeMacBook-Pro.local>
2025-09-08 15:13:57 +0800

    refactor: Simplify arguments in Handle function call

--- hiorder/status_adv.go
+++ hiorder/status_adv.go
@@ -59,7 +59,7 @@ func (e *Engine[T]) doAdvance(
 	if mutSet != nil {
 		mutSet(ordM, data)
 	}
-	_, err = e.mustFsm().Handle(ctx, ordM.Status, event, dict.NewDict().Set("order_id", id), data)
+	_, err = e.mustFsm().Handle(ctx, ordM.Status, event, data)
 	if err != nil {
 		return err
 	}
