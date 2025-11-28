59e60e5dfff685bf8852646d0b22c043bfa63db6
doz <doz@dozdeMacBook-Pro.local>
2025-11-28 10:30:57 +0800

    refactor: Add new functionality to deal processing

--- order/gjjord/deal.go
+++ order/gjjord/deal.go
@@ -2,9 +2,11 @@ package gjjord
 
 import (
 	"context"
+	"fmt"
 	"github.com/hootuu/hyle/hlog"
 	"github.com/hootuu/hyper/hiorder"
 	"github.com/nineora/lightv/lightv"
+	"github.com/nineora/lightv/qing"
 	"github.com/spf13/cast"
 	"go.uber.org/zap"
 )
@@ -46,23 +48,63 @@ func (d *Deal) After(ctx context.Context, src hiorder.Status, target hiorder.Sta
 				zap.Error(err),
 			}
 		})()
-	if target == hiorder.Consensus {
-		if d.ord.Ex.Meta.Get("is_promotion").Bool() {
-			eng, err := GetFactory().Core().Load(ctx, d.ord.ID)
-			if err != nil {
-				return err
-			}
-			return eng.AdvToCompleted(ctx)
-		} else {
-			go func(d *Deal) {
+
+	if d.ord.Ex.Meta.Get("is_promotion").Bool() {
+		eng, err := GetFactory().Core().Load(ctx, d.ord.ID)
+		if err != nil {
+			return err
+		}
+		return eng.AdvToCompleted(ctx)
+	} else {
+		go func(d *Deal) {
+			var awErr error
+			defer hlog.EL(ctx, "gjjord.award.After goroutine").
+				With(
+					zap.Uint64("orderID", d.ord.ID),
+					zap.String("buyer", d.ord.Payer.MustToID()),
+					zap.Uint64("orderAmount", d.ord.Amount),
+					zap.String("biz", d.Code()),
+				).
+				EndWith(func() []zap.Field {
+					return []zap.Field{
+						zap.Error(awErr),
+					}
+				})()
+
+			orderID := d.ord.ID
+			buyer := d.ord.Payer.MustToID()
+			amount := d.ord.Amount
+
+			if target == hiorder.Consensus {
 				cost := cast.ToUint64(d.ord.Ex.Meta.Get("product.cost").Data())
 				totalCost := cost * d.ord.Matter.Count
-				if err := lightv.Assets.AwardByOrder(ctx, d.ord.ID, d.ord.Payer.MustToID(), d.ord.Amount-totalCost, d.Code(), nil); err != nil {
-					hlog.TraceErr("gjjord.Deal.After: AwardByOrder failed", ctx, err)
+				//if awErr := lightv.Assets.AwardByOrder(ctx, orderID, buyer, amount-totalCost, d.Code(), nil); awErr != nil {
+				//	hlog.TraceErr("gjjord.Deal.After: AwardByOrder failed", ctx, awErr)
+				//}
+				if awErr = lightv.AwardOrderPrepare(ctx, orderID, buyer, amount-totalCost, amount, d.Code(), nil); awErr != nil {
+					hlog.TraceErr("gjjord.Deal.After: AwardOrderPrepare failed", ctx, awErr)
+					return
 				}
-			}(d)
-		}
-		return nil
+			}
+
+			if target == hiorder.Refunded {
+				if awErr = lightv.AwardOrderCancel(ctx, orderID, buyer, d.Code(), nil); awErr != nil {
+					hlog.TraceErr("gjjord.Deal.After: AwardOrderCancel failed", ctx, awErr)
+					return
+				}
+
+			}
+			if target == hiorder.Completed {
+				if awErr = lightv.AwardOrderConfirm(ctx, orderID, buyer, d.Code(), nil); awErr != nil {
+					hlog.TraceErr("gjjord.Deal.After: AwardOrderComplete failed", ctx, awErr)
+					return
+				}
+				if awErr = lightv.Assets.TerrTaxing(ctx, cast.ToString(orderID), d.Code(), buyer, amount, nil, 1000, qing.GJJTerrRadioMap); awErr != nil {
+					hlog.TraceFix(fmt.Sprintf("lightv.AwardByOrder: TerrTaxing failed for order %d", orderID), ctx, awErr, zap.Uint64("orderID", orderID))
+					return
+				}
+			}
+		}(d)
 	}
 	return nil
 }
