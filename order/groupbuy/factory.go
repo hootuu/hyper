653142a6de57e55bfb91a372a018136c44350af3
wnjie <13296719323@163.com>
2026-04-13 16:02:42 +0800

    feat(groupbuy): 添加团购订单功能

---
+++ order/groupbuy/factory.go
@@ -0,0 +1,31 @@
+package groupbuy
+
+import (
+	"github.com/hootuu/hyper/hiorder"
+)
+
+const Code = "GROUP_BUY"
+
+type Factory struct {
+	core *hiorder.Factory[Matter]
+}
+
+func newFactory() *Factory {
+	f := &Factory{}
+	f.core = hiorder.NewFactory[Matter](newDealer(Code, f))
+	return f
+}
+
+func (f *Factory) Core() *hiorder.Factory[Matter] {
+	return f.core
+}
+
+var factory *Factory
+
+func GetFactory() *Factory {
+	if factory != nil {
+		return factory
+	}
+	factory = newFactory()
+	return factory
+}
